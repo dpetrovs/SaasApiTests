a8779b276dec2d263d3440f57deaadfc70969dc9
dpetrovs <dspetrovskyy@gmail.com>
2018-03-04 23:35:23 +0200

    Add api Tests

--- ApiTests/Base/BaseClass.cs
+++ ApiTests/Base/BaseClass.cs
@@ -1,5 +1,8 @@
 ﻿
+using Newtonsoft.Json;
+using NUnit.Framework;
 using RestSharp;
+using System.Net;
 
 namespace ApiTests.models
 {
@@ -8,17 +11,141 @@ namespace ApiTests.models
         public const string BaseUrl = "http://159.65.237.98:8182";
         public const string BaseUrlUi = "http://159.65.237.98:80";
         public const string ApplicationUri = "/application.wadl";
-        public const string HealthCheckUri = "/healthcheck";
-        public const string ContactsUri = "/api/v1/contacts";
-        public const string FirstNameBase = "John";
-        public const string LastNameBase = "Doe";
-        public const string EmailBase = "john.doe@unknown.com";
+        public const string HealthCheckUri = "/healthcheck/";
+        public const string ContactsUri = "/api/v1/contacts/";
+        public const string ContentType = "Content-type";
+        public const string ContentTypeValue = "application/json";
+        public const string JsonFirstNameFieldName = "firstName";
+        public const string JsonLastNameFieldName = "lastName";
+        public const string JsonEmalilFieldName = "email";
+        public const string ValidationFirstNameString = "firstName {0} does not match {1}";
+        private const string ValidationLastNameString = "lastName {0} does not match {1}";
+        private const string ValidationEmailString = "email {0} does not match {1}";
+        private const string ValidationIdString = "id {0} does not match {1}";
 
-        public string ResponseContent<T>(RestClient client, RestRequest restRequest) where T : Info
+        public IRestResponse GetApplication()
         {
-            IRestResponse<Info> response = client.Execute<Info>(restRequest);
-            var jsonResponse = response.Content;
-            return jsonResponse;
+            var client = new RestClient(BaseUrlUi);
+            var request = new RestRequest(Method.GET);
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
         }
+
+        public IRestResponse GetHealthCheck()
+        {
+            var client = new RestClient(BaseUrl + HealthCheckUri);
+            var request = new RestRequest(Method.GET);
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
+        }
+
+        public IRestResponse GetContactById(int id)
+        {
+            var client = new RestClient(BaseUrl + ContactsUri + id);
+            var request = new RestRequest(Method.GET);
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
+        }
+
+        public IRestResponse AddNewContact(string firstNameValue, string LastNameValue, string EmailValue)
+        {
+            var client = new RestClient(BaseUrl + ContactsUri);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader(ContentType, ContentTypeValue);
+            request.AddJsonBody(new { firstName = firstNameValue, lastName = LastNameValue, email = EmailValue });
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
+        }
+
+        public IRestResponse ChangeContact(int id, string firstNameValue, string LastNameValue, string EmailValue)
+        {
+            var client = new RestClient(BaseUrl + ContactsUri + id);
+            var request = new RestRequest(Method.PUT);
+            request.AddHeader(ContentType, ContentTypeValue);
+            request.AddJsonBody(new { firstName = firstNameValue, lastName = LastNameValue, email = EmailValue });
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
+        }
+
+        public IRestResponse EditContact(int id, string editableField, string value)
+        {
+            var client = new RestClient(BaseUrl + ContactsUri + id);
+            var request = new RestRequest(Method.PATCH);
+            request.AddHeader(ContentType, ContentTypeValue);
+            request.AddParameter(ContentTypeValue, "{\"" + editableField + "\":\"" + value + "\"}", ParameterType.RequestBody);
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
+        }
+
+        public IRestResponse EditContact(int id, string editableField1, string value1, string editableField2, string value2)
+        {
+            var client = new RestClient(BaseUrl + ContactsUri + id);
+            var request = new RestRequest(Method.PATCH);
+            request.AddHeader(ContentType, ContentTypeValue);
+            request.AddParameter(ContentTypeValue, "{\"" + editableField1 + "\":\"" + value1 + "\"" + ",\n" + "\"" + editableField2 + "\":\"" + value2 + "\"}", ParameterType.RequestBody);
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
+        }
+
+        public IRestResponse DeleteContact(int id)
+        {
+            
+            var client = new RestClient(BaseUrl + ContactsUri + id);
+            var request = new RestRequest(Method.DELETE);
+            IRestResponse<Info> response = client.Execute<Info>(request);
+            return response;
+        }
+
+        public int GetId(string jsonResponse)
+        {
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
+            return rootObject.data[0].id;
+        }
+
+        public string GetFirstName(string jsonResponse)
+        {
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
+            return rootObject.data[0].info.firstName;
+        }
+
+        public string GetLastName(string jsonResponse)
+        {
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
+            return rootObject.data[0].info.lastName;
+        }
+
+        public string GetEmail(string jsonResponse)
+        {
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
+            return rootObject.data[0].info.email;
+        }
+
+        public int GetStatusCodeFromResponseBody(string jsonResponse)
+        {
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
+            return rootObject.status;
+        }
+
+        public void AssertFirstName(string expectedFirstName, string actualFirstName)
+        {
+            Assert.AreEqual(expectedFirstName, actualFirstName, string.Format(ValidationFirstNameString, expectedFirstName, actualFirstName));
+        }
+
+        public void AssertLastName(string expectedLastName, string actualLastName)
+        {
+            Assert.AreEqual(expectedLastName, actualLastName, string.Format(ValidationLastNameString, expectedLastName, actualLastName));
+        }
+
+        public void AssertEmail(string expectedEmail, string actualEmail)
+        {
+            Assert.AreEqual(expectedEmail, actualEmail, string.Format(ValidationEmailString, expectedEmail, actualEmail));
+        }
+
+        public void AssertStatusCode(HttpStatusCode expectedStatusCode, int actualStatusCode)
+        {
+            Assert.AreEqual((int)expectedStatusCode, actualStatusCode, string.Format(ValidationIdString, (int)expectedStatusCode, actualStatusCode));
+        }
+
+
     }
 }
--- ApiTests/tests/SaasTemplateRestApiTest.cs
+++ ApiTests/tests/SaasTemplateRestApiTest.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using NUnit.Framework;
 using RestSharp;
 using System;
+using System.Net;
 
 namespace ApiTests.tests
 {
@@ -10,39 +11,139 @@ namespace ApiTests.tests
     public class SaasTemplateRestApiTest : BaseClass
     {
         [Test]
-        public void GetContact()
+        public void GetApplicationTest()
         {
-            var client = new RestClient();
-            client.BaseUrl = new Uri(BaseUrl);
-            var request = new RestRequest(ContactsUri, Method.GET);
-            IRestResponse<Info> response = client.Execute<Info>(request);
-            var jsonResponse = response.Content;
-            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
-            string firstName = rootObject.data[0].info.firstName;
-            string lastName = rootObject.data[0].info.lastName;
-            string email = rootObject.data[0].info.email;
-            Assert.AreEqual(FirstNameBase, firstName, string.Format("firstName {0} does not match {1}", FirstNameBase, firstName));
-            Assert.AreEqual(LastNameBase, lastName, string.Format("lastName {0} does not match {1}", LastNameBase, lastName));
-            Assert.AreEqual(EmailBase, email, string.Format("Email {0} does not match {1}", EmailBase, email));
+            var getApplication = GetApplication();
+            Assert.IsTrue(getApplication.Content.Contains("Software Engineering Course"));
+            AssertStatusCode(HttpStatusCode.OK, (int)getApplication.StatusCode);
         }
+
         [Test]
-        public void PutContact()
+
+        public void HealthCheckTest()
         {
-            var client = new RestClient(BaseUrl + ContactsUri);
-            var request = new RestRequest();
-            request.Method = Method.POST;
-            request.AddHeader("Accept", "application/json");
-            request.Parameters.Clear();
-            request.AddJsonBody( new { email = "steven.sigal@testmail.com", firstName = "Steven", lastName = "Sigal" });
-            IRestResponse<Info> response = client.Execute<Info>(request);
-            var content = response.Content;
-            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(content);
-            Console.WriteLine(content);
-            //Assert.AreEqual("200", response.StatusCode);
-            string firstName = rootObject.data[0].info.firstName;
-            string lastName = rootObject.data[0].info.lastName;
-            string email = rootObject.data[0].info.email;
-            Console.WriteLine((int) response.StatusCode + " " + firstName + " " + lastName + email);
+            var healthCheck = GetHealthCheck();
+            AssertStatusCode(HttpStatusCode.OK, (int)healthCheck.StatusCode);
+            Assert.IsTrue(healthCheck.Content.Contains("live"));
+        }
+
+        [Test]
+        public void GetContactByIdTest()
+        {
+            string firstName = "John";
+            string lastName = "Doe";
+            string email = "john.doe@unknown.com";
+
+            var getContact = GetContactById(1);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(getContact.Content));
+            AssertFirstName(firstName, GetFirstName(getContact.Content));
+            AssertLastName(lastName, GetLastName(getContact.Content));
+            AssertEmail(email, GetEmail(getContact.Content));
+        }
+
+        [Test]
+        public void AddNewContactTest()
+        {
+            string firstName = "Hulk";
+            string lastName = "Hogan";
+            string email = "hulkHogan@testmail.com";
+
+            var addContact = AddNewContact(firstName, lastName, email);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(addContact.Content));
+            AssertFirstName(firstName, GetFirstName(addContact.Content));
+            AssertLastName(lastName, GetLastName(addContact.Content));
+            AssertEmail(email, GetEmail(addContact.Content));
+        }
+
+        [Test]
+        public void ChangeContactTest()
+        {
+            string firstName = "HulkChanged";
+            string lastName = "HogaChanged1";
+            string email = "hulkHoganChanged@testmail.com";
+
+            var changeContact = ChangeContact(8, firstName, lastName, email);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(changeContact.Content));
+            AssertFirstName(firstName, GetFirstName(changeContact.Content));
+            AssertLastName(lastName, GetLastName(changeContact.Content));
+            AssertEmail(email, GetEmail(changeContact.Content));
+        }
+
+        [Test]
+        public void EditContactFirstNameTest()
+        {
+            string firstName = "HulkEdited";
+
+            var editContact = EditContact(8, JsonFirstNameFieldName, firstName);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(editContact.Content));
+            AssertFirstName(firstName, GetFirstName(editContact.Content));
+        }
+
+        [Test]
+        public void EditContactLastNameTest()
+        {
+            string lastName = "HoganEdited";
+
+            var editContact = EditContact(8, JsonLastNameFieldName, lastName);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(editContact.Content));
+            AssertLastName(lastName, GetLastName(editContact.Content));
+        }
+
+        [Test]
+        public void EditContactEmailTest()
+        {
+            string email = "hulkhogan_edited@testmail.com";
+
+            var editContact = EditContact(8, JsonEmalilFieldName, email);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(editContact.Content));
+            AssertEmail(email, GetEmail(editContact.Content));
+        }
+
+        [Test]
+        public void EditContactFirstNameEmailTest()
+        {
+            string firstName = "Sandra";
+            string email = "sandrabullock@testmail.com";
+
+            var editContact = EditContact(8, JsonEmalilFieldName, email, JsonFirstNameFieldName, firstName);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(editContact.Content));
+            AssertFirstName(firstName, GetFirstName(editContact.Content));
+            AssertEmail(email, GetEmail(editContact.Content));
+        }
+
+        [Test]
+        public void EditContactLastNameEmailTest()
+        {
+            string lastName = "Tarantino";
+            string email = "quentintarantino@testmail.com";
+
+            var editContact = EditContact(3, JsonEmalilFieldName, email, JsonLastNameFieldName, lastName);
+            AssertStatusCode(HttpStatusCode.OK, GetStatusCodeFromResponseBody(editContact.Content));
+            Assert.AreEqual(lastName, GetLastName(editContact.Content));
+            Assert.AreEqual(email, GetEmail(editContact.Content));
+        }
+
+        [Test]
+        public void EditContactFirstLastNameTest()
+        {
+            string firstName = "Luciano";
+            string lastName = "Pavarotti";
+
+            var editContact = EditContact(7, JsonFirstNameFieldName, firstName, JsonLastNameFieldName, lastName);
+            Assert.AreEqual((int)HttpStatusCode.OK, GetStatusCodeFromResponseBody(editContact.Content));
+            Assert.AreEqual(firstName, GetFirstName(editContact.Content));
+            Assert.AreEqual(lastName, GetLastName(editContact.Content));
+        }
+
+        [Test]
+        public void DeleteContactTest()
+        {
+            int id = 3;
+
+            var deleteContact = DeleteContact(id);
+            Assert.AreEqual(HttpStatusCode.OK, deleteContact.StatusCode);
+            var getDeletedContact = GetContactById(id);
+            Assert.AreEqual(HttpStatusCode.NotFound, getDeletedContact.StatusCode);
         }
     }
 }
